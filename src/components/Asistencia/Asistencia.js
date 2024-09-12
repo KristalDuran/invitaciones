e1cb4665c6c1e4dbd92f252e9a6f322052a401d4
kduran <kduran@akurey.com>
2024-09-12 17:54:50 -0600

    fecha

--- src/components/Asistencia/Asistencia.js
+++ src/components/Asistencia/Asistencia.js
@@ -8,7 +8,7 @@ const Asistencia = () => {
     return (
       <div className='asistencia'>
         <h2  className='asistenciaTitle'>Asistencia</h2>
-        <p className='asistenciaInfo'>Saber que estarás con nosotros este día tan especial es muy importante, por esto no olvides confirmar tu asitencia antes del <b>15 de Septiembre.</b></p>
+        <p className='asistenciaInfo'>Saber que estarás con nosotros este día tan especial es muy importante, por esto no olvides confirmar tu asitencia antes del <b>30 de Septiembre.</b></p>
         <Form />
         <p className='regalo'>Tu presencia es lo más importante para nosotros, pero si gustas darnos algún regalo preferimos el mismo en efectivo.</p>
         <p className='despedida'>Te esperamos con tu mejor sonrisa.</p>
